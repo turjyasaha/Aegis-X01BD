a19b4f8d85a98df7cabc648dad81d8f095537d18
tbalden <illespal@gmail.com>
2016-07-16 14:17:25 -0700

    mdss mdp: kcal for mdss_mdp_v1_7
    Adding the kcal interface by @savoca and
    @faux123
    porting from m9 version by @flar2
    
    Using the MDP 1_7 userspace structs to inject
    configurations of kcal to the pp_cache configs.
    Overriding userspace copy with memcpy when
    copy_from_user fails (in case of kcal interface
    used).
    
    Adding a simple mdp ctl struct caching to
    the kcal driver to grab ctl struct for config
    calls.
    
    Could be done nicer with direct interfacing
    to userspace, leaves some space to make the kernel
    crash if a third party app sends faulty configs,
    but there's no security issue or degradation
    of functionality with this memcpy patch.
    
    @tbalden
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: Wahyu Hidayat <xxwahyuxx47@gmail.com>

--- drivers/video/fbdev/msm/mdss_mdp_kcal_ctrl.c
+++ drivers/video/fbdev/msm/mdss_mdp_kcal_ctrl.c
@@ -400,6 +400,7 @@ static ssize_t kcal_enable_store(struct device *dev,
 		mdss_mdp_kcal_update_pcc(lut_data);
 		mdss_mdp_kcal_update_pa(lut_data);
 		//mdss_mdp_kcal_update_igc(lut_data);
+		mdss_mdp_kcal_update_igc(lut_data);
 	} else
 		lut_data->queue_changes = true;
 
@@ -427,6 +428,12 @@ static ssize_t kcal_invert_store(struct device *dev,
 
 	//disable
 	lut_data->invert = 0;
+	lut_data->invert = kcal_invert;
+
+	if (mdss_mdp_kcal_is_panel_on())
+		mdss_mdp_kcal_update_igc(lut_data);
+	else
+		lut_data->queue_changes = true;
 
 	return count;
 }
