1634b9419798a4ae36e49f8683518bdd42e5918e
Wahyu Hidayat <xxwahyuxx47@gmail.com>
2019-08-04 11:02:22 +0700

    Revert "staging: sync: Use an on-stack allocation for fence info ioctl"
    This reverts commit 1bdbafcf0cd431c3c410e2fd1be4061a4481df30.

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -660,8 +660,7 @@ static int sync_fill_pt_info(struct fence *fence, void *data, int size)
 static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 					unsigned long arg)
 {
-	u8 data_buf[4096] __aligned(sizeof(long));
-	struct sync_fence_info_data *data = (typeof(data))data_buf;
+	struct sync_fence_info_data *data;
 	__u32 size;
 	__u32 len = 0;
 	int ret, i;
@@ -679,7 +678,6 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 	if (data == NULL)
 		return -ENOMEM;
 
-#ifdef CONFIG_SYNC_DEBUG
 	strlcpy(data->name, fence->name, sizeof(data->name));
 	data->status = atomic_read(&fence->status);
 	if (data->status >= 0)
@@ -706,6 +704,7 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 		ret = 0;
 
 out:
+	kfree(data);
 
 	return ret;
 }
